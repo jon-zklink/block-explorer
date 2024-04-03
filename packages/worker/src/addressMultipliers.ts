abd30d8ddd21d93434f4502d7f84a347598949e1
nickwest <nickwest@zklink.org>
2024-04-03 04:53:30 -0700

    add address multiplier for hold point

---
+++ packages/worker/src/addressMultipliers.ts
@@ -0,0 +1,11 @@
+export default [
+  {
+    address: "0x0000000000000000000000000000000000000000",
+    multipliers: [
+      {
+        multiplier: 1,
+        timestamp: 0,
+      },
+    ],
+  },
+];
